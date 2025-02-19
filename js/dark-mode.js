5718e62d1f77752afe63df824df3d081b11c0e12
Dev-Ulrich <victorulrich07@gmail.com>
2025-02-19 00:32:52 -0300

    Dark-mode funcional

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,12 +1,18 @@
+let trilho =  document.getElementById('trilho');
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('move');
+});
+
 let darkMode = localStorage.getItem('darkMode');
 const themeSwitch = document.getElementById('trilho');
 const enabledarkMode = ()=>{
     document.body.classList.add('dark-mode');
-    localStorage.setItem('dark-mode', null);
+    localStorage.setItem('dark-mode', 'active');
 }
 const disabledarkMode =()=>{
     document.body.classList.remove('dark-mode');
-    localStorage.setItem('dark-mode', null);
+    localStorage.setItem('dark-mode', 'inactive');
 }
 if(darkMode === 'active') enabledarkMode();
 themeSwitch.addEventListener('click', () =>{
